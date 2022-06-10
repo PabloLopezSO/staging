0c36859a9033ec057ad73388dfca8d5410edaa57
PabloLopezSO <98741854+PabloLopezSO@users.noreply.github.com>
2022-06-10 14:44:26 +0200

    Delete src directory

--- src/test/resources/mySqlSpecific.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'swoacademyjava'@'%' IDENTIFIED BY 'SwoAcademy2022Java!';
-GRANT All on swoacademy2022java.*  TO 'swoacademyjava'@'%';
-
-CREATE USER 'swoacademynet'@'%' IDENTIFIED BY 'SwoAcademy2022Net!';
-GRANT ALL on swoacademy2022net.*  TO 'swoacademynet'@'%';
